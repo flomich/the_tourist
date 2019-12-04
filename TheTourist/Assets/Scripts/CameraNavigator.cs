befe721172e5905dead484fac218e34316447b79
Florian Michelic <florian.michelic@student.tugraz.at>
2019-12-04 20:37:00 +0100

    added camera navigator script, parallax script and dummy street sprite

---
+++ TheTourist/Assets/Scripts/CameraNavigator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraNavigator : MonoBehaviour
+{
+    public GameObject target;
+    public float y_offset = 8.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y + y_offset, -10.0f);
+    }
+}
---
+++ TheTourist/Assets/Scripts/ParallaxScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxScript : MonoBehaviour
+{
+    //the game object
+    public GameObject reference = null;
+
+    //the strength of the parallax effect
+    public float parallax_strength = 0.0f;
+
+    //the offset in x direction
+    public float x_offset = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //make sure that there was a reference set
+        if (!reference) return;
+
+        transform.position = new Vector3(reference.transform.position.x * (1.0f - parallax_strength) + x_offset, transform.position.y, transform.position.z);
+    }
+}

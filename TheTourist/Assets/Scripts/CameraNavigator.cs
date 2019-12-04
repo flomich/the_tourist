bd0982ebed1840dcf7813f48d65cbe4097dfe316
Florian Michelic <florian.michelic@student.tugraz.at>
2019-12-04 20:57:07 +0100

    added cinematic camera movement to camera navigator script

--- TheTourist/Assets/Scripts/CameraNavigator.cs
+++ TheTourist/Assets/Scripts/CameraNavigator.cs
@@ -7,15 +7,31 @@ public class CameraNavigator : MonoBehaviour
     public GameObject target;
     public float y_offset = 8.0f;
 
+    public Vector3 camera_position = new Vector3(0.0f, 0.0f, 0.0f);
+    private Vector3 camera_velocity = new Vector3(0.0f, 0.0f, 0.0f);
+    public float follow_strength = 10.0f;
+    public float follow_drag = 10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(target.transform.position.x, target.transform.position.y + y_offset, -10.0f);
+        Vector3 target_position = new Vector3(0.0f, y_offset, 0.0f);
+        target_position += target.transform.position;
+
+        Vector3 v = target_position - transform.position;
+
+        camera_velocity += v * follow_strength * Time.deltaTime;
+
+        camera_velocity -= camera_velocity * follow_drag * Time.deltaTime;
+
+        camera_position += camera_velocity * Time.deltaTime;
+
+        transform.position = new Vector3(camera_position.x, camera_position.y, -10.0f);
     }
 }

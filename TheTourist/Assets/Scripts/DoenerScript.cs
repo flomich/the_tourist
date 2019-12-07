9a85e8aa9f38e6a2d98856d22cacb2915fc24636
Irena Ruprecht <irena.ruprecht@student.tugraz.at>
2019-12-08 00:15:37 +0100

    added doener to player on collision

--- TheTourist/Assets/Scripts/DoenerScript.cs
+++ TheTourist/Assets/Scripts/DoenerScript.cs
@@ -3,33 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class PuntigamerScript : MonoBehaviour
+public class DoenerScript : MonoBehaviour
 {
 
-    public float max_impact = 7.0f;
-
+    public float max_impact = 10.0f;
     void OnCollisionEnter2D(Collision2D collison)
     {
         movePlayer move_script = collison.otherCollider.gameObject.GetComponent<movePlayer>();
-        if(collison.gameObject.tag.Contains("Player"))
+        if (collison.gameObject.tag.Contains("Player"))
         {
             InventoryScript inventory = collison.gameObject.GetComponent<InventoryScript>();
-            if(inventory != null)
+            if (inventory != null)
             {
-                inventory.addPuntigamerCount(1);
+                inventory.addDoenerCount(1);
                 Destroy(gameObject);
 
             }
         }
         else
         {
-            if(collison.relativeVelocity.magnitude > max_impact)
+            if (collison.relativeVelocity.magnitude > max_impact)
             {
                 SoundEffectScript.Instance.playBreakingGlass(gameObject.transform.position);
                 Destroy(gameObject);
                 Collider2D collider = gameObject.GetComponent<Collider2D>();
                 Vector3 offset = new Vector3(0.0f, collider.bounds.max.y, 0.0f);
-                ParticleEffectsScript.Instance.BeerExplosion(transform.position + offset);
+                //TODO create effect for despawning doener
             }
         }
     }

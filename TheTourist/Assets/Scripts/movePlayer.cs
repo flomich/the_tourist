dadb317debcfee853c781c6bd53a3837d40008fa
Florian Michelic <florian.michelic@student.tugraz.at>
2019-12-07 20:31:01 +0100

    scale animation with movement speed, slowdown movement in air, release space before jump again

--- TheTourist/Assets/Scripts/movePlayer.cs
+++ TheTourist/Assets/Scripts/movePlayer.cs
@@ -19,11 +19,17 @@ public class movePlayer : MonoBehaviour
     //a stride to smoothly stop moving when approaching max speed
     public float move_max_velocity_stride = 1.0f;
 
+    //scales the movement force when in air
+    public float move_air_scale = 0.25f;
+
     //the force which is applied to move the player up
     public float jumping_force = 50.0f;
 
     //the time to apply jump force during one jump
     public float jumping_max_time = 0.1f;
+
+    //scales the animation with the walk speed
+    public float animation_speed_scale = 0.3f;
 
     //the rigidbody of the player
     private Rigidbody2D rigidbody_2d = null;
@@ -33,6 +39,9 @@ public class movePlayer : MonoBehaviour
 
     //flag that indicates if the player can jump (cleared on ground collision)
     private bool jump_cooldown = false;
+
+    //flag that indicates if jump is pressed
+    private bool jump_button_down = false;
 
     void Start()
     {
@@ -54,7 +63,10 @@ public class movePlayer : MonoBehaviour
         Vector3 direction = new Vector3(moveHorizontal, 0.0f, 0.0f);
 
         //flip player based on horizontal input
-        transform.localScale = new Vector3(moveHorizontal < 0.0f ? 0.4f : -0.4f, 0.4f, 0.4f);
+        if (Mathf.Abs(moveHorizontal) > 0.1f)
+        {
+            transform.localScale = new Vector3(moveHorizontal < 0.0f ? 0.4f : -0.4f, 0.4f, 0.4f);
+        }
 
         //if there is movement input
         if (direction.magnitude < 0.0001f)
@@ -64,6 +76,7 @@ public class movePlayer : MonoBehaviour
 
             //set the animation state
             animator.SetInteger("WalkState", 0);
+            animator.speed = 1.0f;
         }
         else
         {
@@ -75,15 +88,19 @@ public class movePlayer : MonoBehaviour
             x /= move_max_velocity_stride;
             x = -(x * x) + 1.0f;
 
-            //add jump force
+            //slow down movement when in air
+            x = current_jump_time > 0.01f ? x * move_air_scale : x;
+
+            //add movement force
             current_movement_force = direction * move_force * x;
 
             //set the animation state
             animator.SetInteger("WalkState", (int)direction.x);
+            animator.speed = 0.1f + Mathf.Abs(rigidbody_2d.velocity.x) * animation_speed_scale;
         }
 
         //jumping
-        if (Input.GetKey(KeyCode.Space) && !jump_cooldown)
+        if (Input.GetKey(KeyCode.Space) && !jump_cooldown && !jump_button_down)
         {
             //increment the jump time
             current_jump_time += Time.deltaTime;
@@ -91,10 +108,22 @@ public class movePlayer : MonoBehaviour
             //add jump force or stop jumping if max jump time is reached
             if (current_jump_time >= jumping_max_time){
                 jump_cooldown = true;
+                jump_button_down = true;
             }
             else{
                 current_movement_force.y += jumping_force;
             }
+        }
+        
+        if(!Input.GetKey(KeyCode.Space))
+        {
+            jump_button_down = false;
+        }
+
+        //play jump animation (to be added)
+        if (current_jump_time > 0.01f)
+        {
+            animator.speed = 1.0f;
         }
 
         //add the force to the physics object

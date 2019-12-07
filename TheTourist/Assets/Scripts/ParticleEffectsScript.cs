090986208f6b21539b73857a6548ff29c84f8b82
Irena Ruprecht <irena.ruprecht@student.tugraz.at>
2019-12-07 21:31:23 +0100

    added puntigamer gameobject and particle effect for puntigamer destroy

---
+++ TheTourist/Assets/Scripts/ParticleEffectsScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEffectsScript : MonoBehaviour
+{
+    /// <summary>
+    /// Singleton
+    /// </summary>
+    public static ParticleEffectsScript Instance;
+
+    public ParticleSystem beerExplosion;
+
+    void Awake()
+    {
+        // Register the singleton
+        if (Instance != null)
+        {
+            Debug.LogError("Multiple instances of ParticleEffectsScript!");
+        }
+
+        Instance = this;
+    }
+
+
+    public void BeerExplosion(Vector3 position)
+    {
+        instantiate(beerExplosion, position);
+    }
+
+    private ParticleSystem instantiate(ParticleSystem prefab, Vector3 position)
+    {
+        ParticleSystem newParticleSystem = Instantiate(
+          prefab,
+          position,
+          Quaternion.identity
+        ) as ParticleSystem;
+
+        newParticleSystem.playbackSpeed = 3.0f;
+
+        // Destroy Particle System when lifetime is over
+        Destroy(newParticleSystem.gameObject,
+          newParticleSystem.main.duration) ;
+
+        return newParticleSystem;
+    }
+}
---
+++ TheTourist/Assets/Scripts/PuntigamerScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuntigamerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+ 
+    void OnCollisionEnter2D(Collision2D collison)
+    {
+        movePlayer move_script = collison.otherCollider.gameObject.GetComponent<movePlayer>();
+        if(collison.gameObject.tag.Contains("Player"))
+        {
+            //Debug.Log("Collision with Player");
+        }
+        else
+        {
+            if(collison.relativeVelocity.magnitude > 15.0f)
+            {
+                Destroy(gameObject);
+                Collider2D collider = gameObject.GetComponent<Collider2D>();
+                Vector3 offset = new Vector3(0.0f, collider.bounds.max.y, 0.0f);
+                ParticleEffectsScript.Instance.BeerExplosion(transform.position + offset);
+            }
+        }
+    }
+}

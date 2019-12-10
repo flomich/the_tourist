2e2a21b92849090d3c5125087c51dd6091b644b1
Markus Perz <markus.perz@student.tugraz.at>
2019-12-10 18:05:04 +0100

    Add Tags to PolaroidView

--- TheTourist/Assets/Scripts/LevelComplete.cs
+++ TheTourist/Assets/Scripts/LevelComplete.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -12,6 +11,41 @@ public class LevelComplete : MonoBehaviour
     public TextMeshPro dateField;
     public TextMeshPro levelNameField;
     public TextMeshPro levelDurationField;
+    public TextMeshPro tagsField;
+
+    public int tagsPerPolaroid = 5;
+    private string[] allTags = {
+        "Graz", "2019", "instagraz",
+        "leiwand", "gdd", "toursim",
+        "Austria", "Styria", "bestcity",
+        "polaroid", "socialmedia"
+    };
+
+    List<string> GetRandomTags(int count) {
+        List<string> all = new List<string>(allTags);
+        if (count > allTags.Length)
+        {
+            return all;
+        }
+        List<string> result = new List<string>();
+        for (int i = 0; i < count; i++) {
+            int selected = (int)UnityEngine.Random.Range(0.0f, all.Count);
+            result.Add(all[selected]);
+            all.RemoveAt(selected);
+        }
+        return result;
+    }
+
+    string GetRandomTagsText() {
+        var tags = GetRandomTags(tagsPerPolaroid);
+        var tagsString = "";
+        bool first = true;
+        tags.ForEach(tag => {
+            tagsString += (first ? "#": ", #") + tag;
+            first = false;
+        });
+        return tagsString;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +56,8 @@ public class LevelComplete : MonoBehaviour
 
         levelNameField.text = levelName;
         selfie.GeneratePose();
+        tagsField.text = GetRandomTagsText();
+        Debug.Log(tagsField.text);
 
         string levelNameForPath = levelName.Replace(' ', '_');
         screenShot.TakeScreenShot(levelNameForPath, path => {

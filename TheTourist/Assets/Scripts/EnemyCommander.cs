9b44ffa62675bab1c263d40f3ae6266ed98d8ce7
Florian Michelic <florian.michelic@student.tugraz.at>
2019-12-11 21:14:02 +0100

    added player commander and basic enemy commander, added simple enemy game object

---
+++ TheTourist/Assets/Scripts/EnemyCommander.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCommander : MonoBehaviour
+{
+    private movePlayer move_script = null;
+
+    void Start()
+    {
+        move_script = GetComponent<movePlayer>();
+    }
+
+    void Update()
+    {
+        float move = Mathf.Sin(Time.time * 0.75f);
+
+        if(Mathf.Abs(move) > 0.5f)
+        {
+            move_script.setMoveInput(move * 0.5f);
+        }
+        else
+        {
+            move_script.setMoveInput(0.0f);
+        } 
+    }
+}

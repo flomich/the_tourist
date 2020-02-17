f50667ce11d2070d6a477c1f06f1142289cda768
Florian Michelic <florian.michelic@student.tugraz.at>
2020-02-17 22:15:24 +0100

    added enemy prefabs, basic enemy ki

---
+++ TheTourist/Assets/Scripts/EnemySetSpritesScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.U2D.Animation;
+
+public class EnemySetSpritesScript : MonoBehaviour
+{
+    public GameObject head;
+    public GameObject body;
+    public GameObject armL;
+    public GameObject armR;
+    public GameObject legL;
+    public GameObject legR;
+
+    public string head_outfit = "0";
+    public string body_outfit = "0";
+    public string leg_outfit = "0";
+
+    void Start()
+    {
+        head.GetComponent<SpriteResolver>().SetCategoryAndLabel("head", head_outfit);
+        body.GetComponent<SpriteResolver>().SetCategoryAndLabel("body", body_outfit);
+        armL.GetComponent<SpriteResolver>().SetCategoryAndLabel("arm_left", body_outfit);
+        armR.GetComponent<SpriteResolver>().SetCategoryAndLabel("arm_right", body_outfit);
+        legL.GetComponent<SpriteResolver>().SetCategoryAndLabel("leg_left", leg_outfit);
+        legR.GetComponent<SpriteResolver>().SetCategoryAndLabel("leg_right", leg_outfit);
+    }
+}

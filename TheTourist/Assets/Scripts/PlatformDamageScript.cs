92d9a87d8dcae5f1101e7f68515f57743127b11f
Irena Ruprecht <irena.ruprecht@student.tugraz.at>
2020-02-20 01:33:47 +0100

    damage platform fix

--- TheTourist/Assets/Scripts/PlatformDamageScript.cs
+++ TheTourist/Assets/Scripts/PlatformDamageScript.cs
@@ -17,10 +17,6 @@ public class PlatformDamageScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -33,21 +29,23 @@ public class PlatformDamageScript : MonoBehaviour
         }
     }
 
+
     private void OnCollisionStay2D(Collision2D collision)
     {
 
-        if(gameObject.tag.Contains("Player") || gameObject.tag.Contains("Enemy"))
+        if(collision.gameObject.tag.Contains("Player") || collision.gameObject.tag.Contains("Enemy"))
         {
+
             // apply force along collision normal
             Rigidbody2D rigidbody = collision.gameObject.GetComponent<Rigidbody2D>();
 
             if (rigidbody != null)
             {
 
-                Vector3 direction = new Vector2(7.0f * Random.Range(-1.0f, 1.0f), 0.0f) *
+                Vector3 direction = new Vector2(7.0f * Random.Range(-1.0f, 1.0f), 7.0f * Random.Range(-1.0f, 1.0f)) *
                     repulsion_force * 0.5f +
-                    -collision.GetContact(0).normal * (repulsion_force);
-                rigidbody.AddForce(direction);
+                    -collision.GetContact(0).normal;
+                rigidbody.AddForce(direction.normalized * (repulsion_force));
 
             }
         }
